014ee86d53e188b73b9e1c0547a2efb78628bedf
xiadd <xiadd0102@gmail.com>
2017-04-28 17:15:45 +0800

    build

---
+++ webpack/dev.js
@@ -0,0 +1,5 @@
+const path = require('path')
+
+const webpack = require('webpack')
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
